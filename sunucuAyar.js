73a2727e815c518bcc44e4ec369ea56e8bcbcc6a
ahmetatesci22 <77553632+ahmetatesci22@users.noreply.github.com>
2021-01-29 18:17:40 +0300

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: '798936171404722186', // sunucu idsi
+  ownerRole:"798975684890067015", // sunucu sahiplerinin rolü
+  teyitsizRolleri:[800066734765113415], // kayıt olmayanlara verieln rol
+  jailRolu:"", // cezalılara verilen rol
+  enAltYetkiliRolu:[800067891063160902], // ilk yetkili permi
+  activity:"Alaska <3", // botun durumu
+  status:"idle",
+  invitelink:"", //davet linki
+  symbol:"〆", // tag
+  voicechannel:"798936171404722190" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
